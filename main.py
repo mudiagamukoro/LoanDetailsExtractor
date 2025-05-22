2a0d05ac7bc61a060fd57297f696373000af83f4
Mudi Mukoro <mudimukoro@gmail.com>
2025-05-22 16:59:39 +0100

    issues

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ async def root():
     """
     return {"message": "Loan Details Extractor API is running"}
 
+
 @app.post("/api/extract-loan-details/")
 async def extract_loan_details(image_file: UploadFile = File(...)):
     """
